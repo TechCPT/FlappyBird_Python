5435d7e725a736b076d592230180d05f0db61c5b
TechCPT <paktungchow04@gmail.com>
2020-12-27 14:21:07 -0500

    Added game vars and basic bird

--- main.py
+++ main.py
@@ -13,13 +13,19 @@ pygame.init()  # initializes all imported pygame modules
 screen = pygame.display.set_mode((288, 512))
 clock = pygame.time.Clock()  # allows the limitation of frame rate
 
+# Game Variables
+gravity = 0.25
+bird_movement = 0
+
 # import surface image and store in bg_surface variable
 # convert isn't strictly necessary but it converts the image into a type of file that is easier to work with for pygame
 bg_surface = pygame.image.load("assets/background-day.png").convert()
 
 floor_surface = pygame.image.load("assets/base.png").convert()
-
 floor_x_pos = 0
+
+bird_surface = pygame.image.load("assets/bluebird-midflap.png").convert()
+bird_rect = bird_surface.get_rect(center=(50, 256))
 
 ''' --- GAME LOOP --- '''
 while True:
@@ -29,9 +35,19 @@ while True:
         if event.type == pygame.QUIT:  # looks for closing of the game with the X button
             pygame.quit()  # uninitializes all pygame modules
             sys.exit()  # shuts down the game completely, ends code
+        if event.type == pygame.KEYDOWN:
+            if event.key == pygame.K_SPACE:
+                bird_movement = 0
+                bird_movement -= 6
+
 
     # Puts one surface on another surface, sets location of top-left corner of surface (x, y) coordinates
     screen.blit(bg_surface, (0, 0))
+
+    bird_movement += gravity
+    bird_rect.centery += bird_movement
+    screen.blit(bird_surface, bird_rect)
+
     floor_x_pos -= 1  # increments x position of floor surface
     draw_floor()
     if floor_x_pos <= -288:
