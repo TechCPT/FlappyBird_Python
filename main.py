f31d135c813f54ef856a9df5514bec43390652de
TechCPT <paktungchow04@gmail.com>
2020-12-27 00:49:05 -0500

    Create continuous floor surface

--- main.py
+++ main.py
@@ -2,15 +2,24 @@
 import pygame
 import sys
 
+
+def draw_floor():
+    screen.blit(floor_surface, (floor_x_pos, 450))
+    screen.blit(floor_surface, (floor_x_pos + 288, 450))
+
+
 pygame.init()  # initializes all imported pygame modules
 # creates display surface stored in the screen variable, passes tuple of width and height as argument
-screen = pygame.display.set_mode((576, 1024))
+screen = pygame.display.set_mode((288, 512))
 clock = pygame.time.Clock()  # allows the limitation of frame rate
 
 # import surface image and store in bg_surface variable
 # convert isn't strictly necessary but it converts the image into a type of file that is easier to work with for pygame
 bg_surface = pygame.image.load("assets/background-day.png").convert()
-bg_surface = pygame.transform.scale2x(bg_surface)  # doubles size of background surface
+
+floor_surface = pygame.image.load("assets/base.png").convert()
+
+floor_x_pos = 0
 
 ''' --- GAME LOOP --- '''
 while True:
@@ -23,6 +32,10 @@ while True:
 
     # Puts one surface on another surface, sets location of top-left corner of surface (x, y) coordinates
     screen.blit(bg_surface, (0, 0))
+    floor_x_pos -= 1  # increments x position of floor surface
+    draw_floor()
+    if floor_x_pos <= -288:
+        floor_x_pos = 0
 
     pygame.display.update()  # Takes anything drawn within the while loop and draws it on the screen
     clock.tick(120)  # limit frame rate, cannot run faster than 120 fps
